de9bab6bbdd8cc41116dcae45d7f670326de8a71
thatsawan2002 <thatsawan.c@ku.th>
2022-06-29 23:11:34 +0700

    Update Cat.java by master

--- Cat.java
+++ Cat.java
@@ -2,6 +2,6 @@ package myapp;
 
 public class Cat {
     public static void main(String[] args){
-        System.out.println("แมวน่ารัก");
+        System.out.println("มังคุดของแมวน่ารักมาก");
     }
 }
