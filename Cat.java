9c220bfa4785eeb660bea78bd879d70c253c27de
thatsawan2002 <thatsawan.c@ku.th>
2022-06-29 22:47:07 +0700

    bring back Cat.java from commit 1bd1888

--- Cat.java
+++ Cat.java
@@ -1,16 +1,6 @@
 package myapp;
 
 public class Cat {
-    private String name;
-    private String age;
-
-    public String getName() {return name;}
-    public String getAge() {return age;}
-
-    public Cat(String name, String age){
-        this.name = name;
-        this.age = age;
-    }
     public static void main(String[] args){
         System.out.println("แมวน่ารัก");
     }
